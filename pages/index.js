9665cd99d766df831cf4e318fd6e64a37a920654
m-irfan244 <irfan.baitham123@gmail.com>
2023-03-25 18:05:17 +0500

    removed custom styling and implement TailwindCSS

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
-import { useState } from 'react';
-import { generateResponse } from './api/openai';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { useState } from "react";
+import { generateResponse } from "./api/openai";
+import Head from "next/head";
 
 function formatTimestamp(timestamp) {
-  return new Intl.DateTimeFormat('en-US', {
-    hour: 'numeric',
-    minute: 'numeric',
+  return new Intl.DateTimeFormat("en-US", {
+    hour: "numeric",
+    minute: "numeric",
   }).format(timestamp);
 }
 
 export default function Home() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [chatHistory, setChatHistory] = useState([]);
 
   async function handleSubmit(e) {
@@ -20,55 +19,47 @@ export default function Home() {
     const currentTime = new Date();
     setChatHistory([
       ...chatHistory,
-      { type: 'user', text: input, timestamp: currentTime },
+      { type: "user", text: input, timestamp: currentTime },
     ]);
-    setInput('');
+    setInput("");
 
     const response = await generateResponse(input);
     setChatHistory([
       ...chatHistory,
-      { type: 'user', text: input, timestamp: currentTime },
-      { type: 'bot', text: response, timestamp: new Date() },
+      { type: "user", text: input, timestamp: currentTime },
+      { type: "bot", text: response, timestamp: new Date() },
     ]);
   }
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>ChatGPT Chatbot</title>
-        <meta name="description" content="A simple ChatGPT chatbot built with Next.js" />
+        <meta
+          name="description"
+          content="A simple ChatGPT chatbot built with Next.js"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className={styles.title}>ChatGPT Chatbot</h1>
-      <div className={styles.chatbox}>
+      <h1>ChatGPT Chatbot</h1>
+      <div>
         {chatHistory.map((message, index) => (
-          <div key={index} className={styles.messageWrapper}>
-            <div className={styles.timestamp}>
-              {formatTimestamp(message.timestamp)}
-            </div>
-            <div
-              className={
-                message.type === 'user'
-                  ? styles.userMessage
-                  : styles.botMessage
-              }
-            >
-              {message.text}
-            </div>
+          <div key={index}>
+            <div>{formatTimestamp(message.timestamp)}</div>
+            <div>{message.text}</div>
           </div>
         ))}
       </div>
-      <form onSubmit={handleSubmit} className={styles.form}>
+      <form onSubmit={handleSubmit} >
         <input
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder="Type your message here..."
-          className={styles.input}
+    
         />
         <button
           type="submit"
-          className={styles.button}
           disabled={!input.trim()}
         >
           Send
